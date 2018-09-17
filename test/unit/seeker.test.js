59e90906b7870e51a10b71e8d7fbc326e7ca1aec
Antreo Pukay <acrosswalker@yahoo.com>
2018-09-17 14:00:57 -0700

    seeker unit test passing

--- test/unit/seeker.test.js
+++ test/unit/seeker.test.js
@@ -2,6 +2,12 @@ const { assert } = require('chai');
 const { Types } = require('mongoose');
 const Seeker = require('../../lib/models/seeker');
 
+const getErrors = (validation, numberExpected) => {
+    assert.isDefined(validation);
+    const errors = validation.errors;
+    assert.equal(Object.keys(errors).length, numberExpected);
+    return errors;
+};
 
 describe.only('User model', () => {
     
@@ -17,17 +23,16 @@ describe.only('User model', () => {
         };
 
         const seeker = new Seeker(data);
-
         const json = seeker.toJSON();
         delete json._id;
-        assert.deepEqual(json, data);
-        // console.log('*** json', json);
-        // console.log('*** data', data);
+        assert.deepEqual(json, data);       
     });
 
     it('validate required fields', () => {
-        const seeker = new Seeker;
+        const seeker = new Seeker({});
+        const errors = getErrors(seeker.validateSync(), 3);
+        assert.equal(errors.name.kind, 'required');
+        assert.equal(errors.activity.kind, 'required');
+        assert.equal(errors.zip.kind, 'required');
     });
-
-
 });
