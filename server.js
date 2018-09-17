ee507fceca04b1c6bc975e09a48979821566f6c5
Easton Gorishek <ieaston26@gmail.com>
2018-09-17 06:37:01 -0700

    more heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/rehome
 connect(MONGODB_URI);
 
 const server = http.createServer(app);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 server.listen(port, () => {
     // eslint-disable-next-line
